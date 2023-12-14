b0f4d296806b41b15fc6f7ca62c907ee24516f55
Filip Klimeš <filip@filipklimes.cz>
2023-12-14 07:37:08 +0100

    2023 day15

---
+++ src/y2023/day15.rs
@@ -0,0 +1,78 @@
+use std::fs;
+
+pub(crate) fn day15() {
+    println!("{}", part_a(fs::read_to_string("input/2023/day15/input.txt").unwrap()));
+    println!("{}", part_b(fs::read_to_string("input/2023/day15/input.txt").unwrap()));
+}
+
+fn part_a(input: String) -> u32 {
+    input.split(",").map(|seq| {
+        let mut ans = 0;
+        for c in seq.chars() {
+            let ci = c as u32;
+            ans += ci;
+            ans *= 17;
+            ans = ans % 256;
+        }
+        ans
+    }).sum()
+}
+
+fn part_b(input: String) -> usize {
+    let mut boxes: Vec<Vec<(&str, usize)>> = vec! { vec!(); 256 };
+    input.split(",").for_each(|seq| {
+        let mut label = String::new();
+        let mut ans = 0;
+        for c in seq.chars() {
+            if c == '-' || c == '=' { break; }
+            label.push(c);
+
+            let ci = c as usize;
+            ans += ci;
+            ans *= 17;
+            ans = ans % 256;
+        }
+
+        if seq.ends_with("-") {
+            boxes[ans] = boxes[ans].iter()
+                .filter(|(l2, _)| l2 != &label.as_str())
+                .map(|v| *v)
+                .collect();
+        } else {
+            let (label, focal_length) = seq.split_once("=").unwrap();
+            if let Some(pos) = boxes[ans].iter().position(|(l2, _)| l2 == &label) {
+                boxes[ans][pos] = (label, focal_length.parse::<usize>().unwrap());
+            } else {
+                boxes[ans].push((label, focal_length.parse::<usize>().unwrap()));
+            }
+        }
+    });
+
+    let mut ans = 0;
+    for box_i in 0..256 {
+        for slot_i in 0..boxes[box_i].len() {
+            let (_label, focal_length) = boxes[box_i][slot_i];
+            ans += (box_i + 1) * (slot_i + 1) * focal_length;
+        }
+    }
+    ans
+}
+
+#[cfg(test)]
+mod day15_tests {
+    use std::fs;
+
+    use crate::y2023::day15::{part_a, part_b};
+
+    #[test]
+    fn test_works() {
+        assert_eq!(1320, part_a(fs::read_to_string("input/2023/day15/test.txt").unwrap()));
+        assert_eq!(145, part_b(fs::read_to_string("input/2023/day15/test.txt").unwrap()));
+    }
+
+    #[test]
+    fn input_works() {
+        assert_eq!(495972, part_a(fs::read_to_string("input/2023/day15/input.txt").unwrap()));
+        assert_eq!(245223, part_b(fs::read_to_string("input/2023/day15/input.txt").unwrap()));
+    }
+}
--- src/y2023/mod.rs
+++ src/y2023/mod.rs
@@ -18,6 +18,7 @@ use crate::y2023::day11::day11;
 use crate::y2023::day12::day12;
 use crate::y2023::day13::day13;
 use crate::y2023::day14::day14;
+use crate::y2023::day15::day15;
 
 mod day01;
 mod day02;
@@ -33,6 +34,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
 pub(crate) fn y2023() {
     let args: Vec<String> = env::args().collect();
@@ -109,5 +111,10 @@ pub(crate) fn y2023() {
     if args.contains(&"all".to_string()) || args.contains(&"day14".to_string()) {
         println!("{}", format!("--- day14:").underline().green());
         measure!(day14());
+    }
+
+    if args.contains(&"all".to_string()) || args.contains(&"day15".to_string()) {
+        println!("{}", format!("--- day15:").underline().green());
+        measure!(day15());
     }
 }
