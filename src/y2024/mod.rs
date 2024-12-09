cdfad48367f0f9d751e9ee588883e52d4c05f2d7
Filip Klimeš <filip@filipklimes.cz>
2024-12-09 08:51:38 +0100

    2024 day10

--- src/y2024/mod.rs
+++ src/y2024/mod.rs
@@ -13,6 +13,7 @@ use crate::y2024::day06::day06;
 use crate::y2024::day07::day07;
 use crate::y2024::day08::day08;
 use crate::y2024::day09::day09;
+use crate::y2024::day10::day10;
 
 mod day01;
 mod day02;
@@ -23,6 +24,7 @@ mod day06;
 mod day07;
 mod day08;
 mod day09;
+mod day10;
 
 pub(crate) fn y2024() {
     let args: Vec<String> = env::args().collect();
@@ -74,5 +76,10 @@ pub(crate) fn y2024() {
     if args.contains(&"all".to_string()) || args.contains(&"day09".to_string()) {
         println!("{}", format!("--- day09:").underline().green());
         measure!(day09());
+    }
+
+    if args.contains(&"all".to_string()) || args.contains(&"day10".to_string()) {
+        println!("{}", format!("--- day10:").underline().green());
+        measure!(day10());
     }
 }
