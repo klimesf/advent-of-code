b1b9e835bf50f7f5e776c1910a70f94083dac9f0
Filip Klimeš <filip@filipklimes.cz>
2024-12-13 16:09:07 +0100

    2024 day13 algebra

--- src/y2024/day13.rs
+++ src/y2024/day13.rs
@@ -5,7 +5,42 @@ pub(crate) fn day13() {
     println!("{}", part_b(fs::read_to_string("input/2024/day13/input.txt").unwrap()));
 }
 
-fn part_a(input: String) -> usize {
+fn part_a(input: String) -> i64 {
+    let configurations = parse_input(input);
+    let mut ans = 0;
+    for (xa, ya, xb, yb, xp, yp) in configurations {
+        let a = (xp * yb - yp * xb) / (xa * yb - ya * xb);
+        let b = (yp - a * ya) / yb;
+        let x = xa * a + xb * b;
+        let y = ya * a + yb * b;
+
+        if a > 0 && a <= 100 && b > 0 && b <= 100 && x == xp && y == yp {
+            ans += 3 * a + b;
+        }
+    }
+    ans
+}
+
+fn part_b(input: String) -> i64 {
+    let configurations = parse_input(input);
+    let mut ans = 0;
+    for (xa, ya, xb, yb, xp, yp) in configurations {
+        let xp = xp + 10000000000000;
+        let yp = yp + 10000000000000;
+
+        let a = (xp * yb - yp * xb) / (xa * yb - ya * xb);
+        let b = (yp - a * ya) / yb;
+        let x = xa * a + xb * b;
+        let y = ya * a + yb * b;
+
+        if a >= 0 && b >= 0 && x == xp && y == yp {
+            ans += 3 * a + b;
+        }
+    }
+    ans
+}
+
+fn parse_input(input: String) -> Vec<(i64, i64, i64, i64, i64, i64)> {
     let mut configurations = vec!();
     input.split("\n\n").for_each(|block| {
         let lines = block.lines().collect::<Vec<&str>>();
@@ -15,82 +50,71 @@ fn part_a(input: String) -> usize {
 
         let (sxa, sya) = s_a.split_once(", ").unwrap();
         let (_, ssxa) = sxa.split_once("+").unwrap();
-        let xa = ssxa.parse::<usize>().unwrap();
+        let xa = ssxa.parse::<i64>().unwrap();
         let (_, ssya) = sya.split_once("+").unwrap();
-        let ya = ssya.parse::<usize>().unwrap();
+        let ya = ssya.parse::<i64>().unwrap();
 
         let (sxb, syb) = s_b.split_once(", ").unwrap();
         let (_, ssxb) = sxb.split_once("+").unwrap();
-        let xb = ssxb.parse::<usize>().unwrap();
+        let xb = ssxb.parse::<i64>().unwrap();
         let (_, ssyb) = syb.split_once("+").unwrap();
-        let yb = ssyb.parse::<usize>().unwrap();
+        let yb = ssyb.parse::<i64>().unwrap();
 
         let (sxp, syp) = s_p.split_once(", ").unwrap();
         let (_, ssxp) = sxp.split_once("=").unwrap();
-        let xp = ssxp.parse::<usize>().unwrap();
+        let xp = ssxp.parse::<i64>().unwrap();
         let (_, ssyp) = syp.split_once("=").unwrap();
-        let yp = ssyp.parse::<usize>().unwrap();
+        let yp = ssyp.parse::<i64>().unwrap();
 
         configurations.push((xa, ya, xb, yb, xp, yp));
     });
-
-    let mut ans = 0;
-    for (xa, ya, xb, yb, xp, yp) in configurations {
-        let mut min = usize::MAX;
-
-        for a in 0..=100 {
-            if xa * a > xp { continue; }
-            if ya * a > yp { continue; }
-            for b in 0..=100 {
-                if xa * a + xb * b > xp { continue; }
-                if ya * a + yb * b > yp { continue; }
-                if xa * a + xb * b == xp && ya * a + yb * b == yp && 3 * a + b < min {
-                    min = 3 * a + b;
-                }
-            }
-        }
-
-        if min < usize::MAX {
-            ans += min;
-        }
-    }
-
-    ans
+    configurations
 }
 
-fn part_b(_: String) -> usize {
-    // Preprocessed in rust, solved in python:
-    // from z3 import *
-    //
-    // def solve(xa, ya, xb, yb, xp, yp):
-    //     a = Int('a')
-    //     b = Int('b')
-    //     cost = Int('cost')
-    //
-    //     opt = Optimize()
-    //
-    //     opt.add(a * xa + b * xb == xp)
-    //     opt.add(a * ya + b * yb == yp)
-    //     opt.add(cost == 3 * a + b)
-    //
-    //     h = opt.minimize(cost)
-    //     if opt.check() == sat:
-    //         model = opt.model()
-    //         sol_a = model[a].as_long()
-    //         sol_b = model[b].as_long()
-    //         return 3 * sol_a + sol_b
-    //     else:
-    //         return 0
-    //
-    // input = open('input/2024/day13/input_parsed.txt').read().split('\n')
-    // ans = 0
-    // for line in input:
-    //     nums = line.split()
-    //     ans += solve(int(nums[0]), int(nums[1]), int(nums[2]), int(nums[3]), int(nums[4]) + 10000000000000, int(nums[5]) + 10000000000000)
-    //
-    // print(ans)
-    92572057880885
-}
+// --- Equations:
+// a * xa + b * xb == xp
+// a * ya + b * yb == yp
+//
+// b == (yp - a * ya) / yb
+//
+// a * xa + ((yp - a * ya) / yb) * xb == xp
+// a * xa + (((yp - a * ya) * xb) / yb) == xp
+// a * xa + ((yp * xb - a * ya * xb) / yb) == xp
+// a * xa * yb + yp * xb - a * ya * xb == xp * yb
+// a * xa * yb - a * ya * xb == xp * yb - yp * xb
+// a * (xa * yb - ya * xb) == xp * yb - yp * xb
+// a = (xp * yb - yp * xb) / (xa * yb - ya * xb)
+
+// --- Python solution used for part 2 initially:
+// from z3 import *
+//
+// def solve(xa, ya, xb, yb, xp, yp):
+//     a = Int('a')
+//     b = Int('b')
+//     cost = Int('cost')
+//
+//     opt = Optimize()
+//
+//     opt.add(a * xa + b * xb == xp)
+//     opt.add(a * ya + b * yb == yp)
+//     opt.add(cost == 3 * a + b)
+//
+//     h = opt.minimize(cost)
+//     if opt.check() == sat:
+//         model = opt.model()
+//         sol_a = model[a].as_long()
+//         sol_b = model[b].as_long()
+//         return 3 * sol_a + sol_b
+//     else:
+//         return 0
+//
+// input = open('input/2024/day13/input_parsed.txt').read().split('\n')
+// ans = 0
+// for line in input:
+//     nums = line.split()
+//     ans += solve(int(nums[0]), int(nums[1]), int(nums[2]), int(nums[3]), int(nums[4]) + 10000000000000, int(nums[5]) + 10000000000000)
+//
+// print(ans)
 
 #[cfg(test)]
 mod day13_tests {
