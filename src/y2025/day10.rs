9865169f52c3c6951682aec424028c66b49124ca
Filip Klimeš <filip@filipklimes.cz>
2025-12-10 14:38:07 +0100

    2025 day10 but better

--- src/y2025/day10.rs
+++ src/y2025/day10.rs
@@ -1,70 +1,52 @@
-use std::cmp::Ordering;
-use std::collections::{BinaryHeap};
+use std::collections::{VecDeque};
 use std::fs;
 use rayon::iter::ParallelIterator;
 use rayon::iter::IntoParallelRefIterator;
+use z3::{Optimize, ast::Int, SatResult};
 
 pub(crate) fn day10(print: fn(usize)) {
-    // print(part_a(fs::read_to_string("input/2025/day10/input.txt").unwrap()));
-    print(part_b(fs::read_to_string("input/2025/day10/test.txt").unwrap()));
+    print(part_a(fs::read_to_string("input/2025/day10/input.txt").unwrap()));
+    print(part_b(fs::read_to_string("input/2025/day10/input.txt").unwrap()));
 }
 
 fn part_a(input: String) -> usize {
-    let mut total = 0;
-    input.lines().for_each(|line| {
+    let lines: Vec<&str> = input.lines().collect();
+    lines.par_iter().map(|line| {
         let parts = line.split_whitespace().collect::<Vec<&str>>();
 
-        let target_str = parts[0][1..parts[0].len() - 1].to_string();
-        let target = target_str.chars().map(|c| {
-            if c == '.' { false } else { true }
-        }).collect::<Vec<bool>>();
+        let target_chars = parts[0][1..parts[0].len() - 1].chars().collect::<Vec<char>>();
+        let mut target = 0;
+        for i in 0..target_chars.len() {
+            if target_chars[i] != '#' {
+                continue;
+            }
+            target |= 0b1 << i; 
+        }
 
-        let presses = parts[1..parts.len() - 1].iter().map(|part| {
-            part[1..part.len() - 1].split(",").map(|part| part.parse::<usize>().unwrap()).collect::<Vec<usize>>()
-        }).collect::<Vec<Vec<usize>>>();
+        let buttons = parts[1..parts.len() - 1].iter().map(|part| {
+            part[1..part.len() - 1]
+                .split(",")
+                .map(|part| part.parse::<usize>().unwrap())
+                .map(|num| 0b1 << num )
+                .reduce(|a, b| a | b)
+                .unwrap_or(0)
+        }).collect::<Vec<usize>>();
 
-        let min = solve_a(target, presses);
-        total += min;
-    });
-    total
+        solve_a(target, buttons)
+    }).sum()
 }
 
-fn solve_a(target: Vec<bool>, presses: Vec<Vec<usize>>) -> usize {
-    let mut heap = BinaryHeap::new();
-    heap.push(StateA { state: vec![false; target.len()], presses: 0 });
-    
-    while let Some(next_state) = heap.pop() {
-        if next_state.state == target { return next_state.presses }
-        
-        for press in presses.iter() {
-            let mut new_state = next_state.state.clone();
-            for i in 0..press.len() {
-                let pos = press[i];
-                new_state[pos] = !new_state[pos];
-            }
-            let bump = StateA { state: new_state.clone(), presses: next_state.presses + 1 };
-            heap.push(bump);
+fn solve_a(target: usize, buttons: Vec<usize>) -> usize {
+    let mut queue = VecDeque::new();
+    queue.push_back((0b0, 0));
+
+    while let Some((state, presses)) = queue.pop_front() {
+        if state == target { return presses; }
+        for button in buttons.iter() {
+            queue.push_back((state ^ button, presses + 1));
         }
     }
     0
-}
-
-#[derive(Eq, PartialEq)]
-struct StateA {
-    state: Vec<bool>,
-    presses: usize,
-}
-
-impl Ord for StateA {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.presses.cmp(&self.presses)
-    }
-}
-
-impl PartialOrd for StateA {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
 }
 
 fn part_b(input: String) -> usize {
@@ -72,76 +54,76 @@ fn part_b(input: String) -> usize {
     lines.par_iter().map(|line| {
         let parts = line.split_whitespace().collect::<Vec<&str>>();
 
-        let presses = parts[1..parts.len() - 1].iter().map(|part| {
-            part[1..part.len() - 1].split(",").map(|part| part.parse::<usize>().unwrap()).collect::<Vec<usize>>()
+        let buttons = parts[1..parts.len() - 1].iter().map(|part| {
+            part[1..part.len() - 1].split(",")
+                .map(|part| part.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
         }).collect::<Vec<Vec<usize>>>();
-        
-        let joltage_str = parts[parts.len() - 1][1..parts[parts.len() - 1].len() - 1].to_string();
-        let joltage = joltage_str.split(",").map(|part| part.parse::<usize>().unwrap()).collect::<Vec<usize>>();
 
-        let min = solve_b(joltage, presses);
-        min
+        let joltage_str = parts[parts.len() - 1][1..parts[parts.len() - 1].len() - 1].to_string();
+        let joltage = joltage_str.split(",")
+            .map(|part| part.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        solve_b(joltage, buttons)
     }).sum()
 }
 
-// Solved by Z3 first
-fn solve_b(joltage: Vec<usize>, presses: Vec<Vec<usize>>) -> usize {
-    let mut heap = BinaryHeap::new();
-    heap.push(StateB {
-        joltage: vec![0; joltage.len()],
-        presses: 0,
-        dist: calc_dist(&joltage, &vec![0; joltage.len()], 0),
-    });
+// a * (p1, p2, p3, ...) + b * (p1, p2, p3, ...) + c * (p1, p2, p3, ...) + ... = (P1, P2, P3, ...)
+// a * p1 + b * p1 + ... = P1
+// a * p2 + b * p2 + ... = P2
+//
+// a >= 0, b >= 0, ...
+//
+// MIN(a + b + c + ... )
+fn solve_b(target: Vec<usize>, buttons: Vec<Vec<usize>>) -> usize {
+    let opt = Optimize::new();
 
-    'outer: while let Some(next_state) = heap.pop() {
-        if next_state.joltage == joltage { return next_state.presses }
-        for i in 0..joltage.len() {
-            if next_state.joltage[i] > joltage[i] { continue 'outer; }
-        }
+    let mut button_clicks = vec!();
+    for i in 0..buttons.len() {
+        let variable = Int::fresh_const(format!("button{}", i).as_str());
+        button_clicks.push(variable);
+    }
 
-        for press in presses.iter() {
-            let mut new_joltage = next_state.joltage.clone();
-            for i in 0..press.len() {
-                let pos = press[i];
-                new_joltage[pos] += 1;
+    for i in 0..buttons.len() {
+        opt.assert(&button_clicks[i].ge(0));
+    }
+
+    for i in 0..target.len() {
+        // a * pi + b * pi + ... = Pi
+        let left_side = buttons.iter().enumerate()
+            .map(|(j, button)| {
+                if button.contains(&i) {
+                    Int::from_u64(1) * button_clicks[j].clone()
+                } else {
+                    Int::from_u64(0) * button_clicks[j].clone()
+                }
+            })
+            .reduce(|a, b| a + b)
+            .unwrap();
+        let right_side = Int::from_u64(target[i] as u64);
+        let equation = left_side.eq(&right_side);
+        opt.assert(&equation);
+    }
+
+    let objective = button_clicks.iter()
+        .map(|click| click.clone())
+        .reduce(|a, b| a + b)
+        .unwrap();
+    opt.minimize(&objective);
+
+    match opt.check(&[]) {
+        SatResult::Sat => {
+            let mut total = 0;
+            let model = opt.get_model().unwrap();
+            for i in 0..button_clicks.len() {
+                let click = &button_clicks[i];
+                total += model.eval(click, true).unwrap().as_i64().unwrap();
             }
-            let bump = StateB {
-                joltage: new_joltage.clone(),
-                presses: next_state.presses + 1,
-                dist: calc_dist(&joltage, &new_joltage, next_state.presses + 1),
-            };
-            heap.push(bump);
+            total as usize
         }
-    }
-    0
-}
-
-fn calc_dist(joltage: &Vec<usize>, joltage_state: &Vec<usize>, _presses: usize) -> usize {
-    let mut dist = 0;
-    for i in 0..joltage.len() {
-        let a = joltage_state[i];
-        let b = joltage[i];
-        dist += a.max(b) - a.min(b);
-    }
-    dist
-}
-
-#[derive(Eq, PartialEq)]
-struct StateB {
-    joltage: Vec<usize>,
-    presses: usize,
-    dist: usize,
-}
-
-impl Ord for StateB {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.dist.cmp(&self.dist)
-    }
-}
-
-impl PartialOrd for StateB {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+        SatResult::Unknown => { 0 }
+        SatResult::Unsat => { 0 }
     }
 }
 
@@ -160,6 +142,6 @@ mod day10_tests {
     #[test]
     fn input_works() {
         assert_eq!(498, part_a(fs::read_to_string("input/2025/day10/input.txt").unwrap()));
-        assert_eq!(0, part_b(fs::read_to_string("input/2025/day10/input.txt").unwrap()));
+        assert_eq!(17133, part_b(fs::read_to_string("input/2025/day10/input.txt").unwrap()));
     }
 }
