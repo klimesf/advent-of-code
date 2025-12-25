71080274d84d3719ffb32e16df4e5cb3e684e977
Filip Klimeš <filip@filipklimes.cz>
2025-12-25 17:38:09 +0100

    2020 day23 but better

--- src/y2020/day23.rs
+++ src/y2020/day23.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools;
-use std::collections::VecDeque;
 use std::fs;
 
 pub(crate) fn day23() {
@@ -8,20 +7,27 @@ pub(crate) fn day23() {
 }
 
 fn part_a(input: String) -> usize {
-    let mut cups = input
+    let cups = input
         .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect::<VecDeque<u32>>();
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<usize>>();
 
-    let len = cups.len() as u32;
+    let len = cups.len();
+    let mut current = cups[0];
+    let mut links = vec![0; len + 1];
+
+    cups.iter().circular_tuple_windows().for_each(|(c1, c2)| {
+        links[*c1] = *c2;
+    });
 
     for _ in 0..100 {
-        let current = cups.pop_front().unwrap();
-        let r1 = cups.pop_front().unwrap();
-        let r2 = cups.pop_front().unwrap();
-        let r3 = cups.pop_front().unwrap();
-        let rs = [r1, r2, r3];
+        let r1 = links[current];
+        let r2 = links[r1];
+        let r3 = links[r2];
 
+        links[current] = links[r3];
+
+        let rs = [r1, r2, r3];
         let mut label = current;
         loop {
             label = label - 1;
@@ -33,61 +39,47 @@ fn part_a(input: String) -> usize {
             }
         }
 
-        cups.push_front(current);
-        let mut rotations = 0;
-        while cups[0] != label {
-            let a = cups.pop_back().unwrap();
-            cups.push_front(a);
-            rotations += 1;
-        }
-        cups.pop_front().unwrap();
-        cups.push_front(r3);
-        cups.push_front(r2);
-        cups.push_front(r1);
-        cups.push_front(label);
-        for _ in 0..=(rotations + 3) {
-            let a = cups.pop_front().unwrap();
-            cups.push_back(a);
-        }
+        let swap = links[label];
+        links[label] = r1;
+        links[r3] = swap;
+
+        current = links[current];
     }
 
-    while cups[0] != 1 {
-        let a = cups.pop_front().unwrap();
-        cups.push_back(a);
+    let mut s = String::new();
+    let mut b = links[1];
+    while b != 1 {
+        s.push_str(b.to_string().as_str());
+        b = links[b];
     }
-
-    cups.iter()
-        .skip(1)
-        .map(|c| c.to_string())
-        .join("")
-        .parse::<usize>()
-        .unwrap()
+    s.parse::<usize>().unwrap()
 }
 
-fn part_b(input: String) -> u32 {
+fn part_b(input: String) -> usize {
     let mut cups = input
         .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect::<VecDeque<u32>>();
-    cups.reserve(100_000_000);
-
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<usize>>();
+    cups.reserve(1_000_000 - cups.len());
     for i in cups.len() + 1..=1_000_000 {
-        cups.push_back(i as u32);
+        cups.push(i);
     }
-    let len = cups.len() as u32;
+    let len = cups.len();
+    let mut current = cups[0];
+    let mut links = vec![0; len + 1];
 
-    for i in 0..10_000_000 {
-        if i % 10000 == 0 {
-            println!("{}", i);
-            let pos = cups.iter().position(|&c| c == 1).unwrap();
-            println!("{} {} {}", pos, cups[pos + 1 % cups.len()], cups[pos + 2 % cups.len()]);
-        }
-        let current = cups.pop_front().unwrap();
-        let r1 = cups.pop_front().unwrap();
-        let r2 = cups.pop_front().unwrap();
-        let r3 = cups.pop_front().unwrap();
+    cups.iter().circular_tuple_windows().for_each(|(c1, c2)| {
+        links[*c1] = *c2;
+    });
+
+    for _ in 0..10_000_000 {
+        let r1 = links[current];
+        let r2 = links[r1];
+        let r3 = links[r2];
+
+        links[current] = links[r3];
+
         let rs = [r1, r2, r3];
-
         let mut label = current;
         loop {
             label = label - 1;
@@ -99,34 +91,14 @@ fn part_b(input: String) -> u32 {
             }
         }
 
-        cups.push_front(current);
-        let mut rotations = 0;
-        while cups[0] != label {
-            let a = cups.pop_back().unwrap();
-            cups.push_front(a);
-            rotations += 1;
-        }
-        // println!("over! lab:{} i:{} rots:{}", label, i, rotations);
-        if i > 100 && rotations != i + 2 {
-            panic!("lab:{} i:{} rots:{}", label, i, rotations);
-        }
-        cups.pop_front().unwrap();
-        cups.push_front(r3);
-        cups.push_front(r2);
-        cups.push_front(r1);
-        cups.push_front(label);
-        for _ in 0..=(rotations + 3) {
-            let a = cups.pop_front().unwrap();
-            cups.push_back(a);
-        }
+        let swap = links[label];
+        links[label] = r1;
+        links[r3] = swap;
+
+        current = links[current];
     }
 
-    while cups[0] != 1 {
-        let a = cups.pop_front().unwrap();
-        cups.push_back(a);
-    }
-
-    cups[1] * cups[2]
+    links[1] * links[links[1]]
 }
 
 #[cfg(test)]
@@ -138,14 +110,12 @@ mod day23_tests {
     #[test]
     fn test_works() {
         assert_eq!(67384529, part_a(fs::read_to_string("input/2020/day23/test.txt").unwrap()));
-        // assert_eq!(0, part_b(fs::read_to_string("input/2020/day23/test.txt").unwrap()));
-        // 3683490560
-        // 111057672960
+        assert_eq!(149245887792, part_b(fs::read_to_string("input/2020/day23/test.txt").unwrap()));
     }
 
     #[test]
     fn input_works() {
         assert_eq!(58427369, part_a(fs::read_to_string("input/2020/day23/input.txt").unwrap()));
-        assert_eq!(0, part_b(fs::read_to_string("input/2020/day23/input.txt").unwrap()));
+        assert_eq!(111057672960, part_b(fs::read_to_string("input/2020/day23/input.txt").unwrap()));
     }
 }
